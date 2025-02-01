89c402bcb00e684210f47d9002ea4dd94a1ff9f0
Loke-Automation <lokeshkumar1212@gmail.com>
2025-02-01 22:09:27 +0530

    Add files via upload
    day1

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		System.out.println("This is my 1st code");
+		// TODO Auto-generated method stub
+
+	}
+
+}
